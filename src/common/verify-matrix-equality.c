b475b32e38f96b7bc9a942b4b2bd0b92c2070d64
thorbert-anson-shi <tobyas2005139@gmail.com>
2026-05-23 02:12:09 +0700

    debug print for correctness verification

--- src/common/verify-matrix-equality.c
+++ src/common/verify-matrix-equality.c
@@ -1,9 +1,11 @@
 #include <math.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 bool verify_matrix_equality(float *a, float *b, int len) {
   for (int i = 0; i < len; i++) {
     if (fabs(a[i] - b[i]) > 0.01) {
+      printf("%f %f", a[i], b[i]);
       return false;
     }
   }
