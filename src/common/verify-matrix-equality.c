867018fd892e8254d0d040f412997206388aeab2
thorbert-anson-shi <tobyas2005139@gmail.com>
2026-05-23 02:07:19 +0700

    fabs

--- src/common/verify-matrix-equality.c
+++ src/common/verify-matrix-equality.c
@@ -1,8 +1,9 @@
+#include <math.h>
 #include <stdbool.h>
 
 bool verify_matrix_equality(float *a, float *b, int len) {
   for (int i = 0; i < len; i++) {
-    if (abs(a[i] - b[i]) > 0.01) {
+    if (fabs(a[i] - b[i]) > 0.01) {
       return false;
     }
   }
