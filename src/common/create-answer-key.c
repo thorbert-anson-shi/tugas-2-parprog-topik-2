1ce4d9c3a4ad7300a346d57e468131a4ed899840
thorbert-anson-shi <tobyas2005139@gmail.com>
2026-05-22 22:55:51 +0700

    fix: cudaMalloc pointer type

--- src/common/create-answer-key.c
+++ src/common/create-answer-key.c
@@ -1,5 +1,6 @@
 #include <cublas_v2.h>
 #include <math.h>
+#include <stdlib.h>
 
 void create_answer_key(float *answer_key, int n) {
   int arr_len = pow(n, 2);
@@ -11,9 +12,9 @@ void create_answer_key(float *answer_key, int n) {
   float beta = 0.0f;
 
   float *t_a, *t_b, *t_c;
-  cudaMalloc(&t_a, arr_len * sizeof(float));
-  cudaMalloc(&t_b, arr_len * sizeof(float));
-  cudaMalloc(&t_c, arr_len * sizeof(float));
+  cudaMalloc((void **)&t_a, arr_len * sizeof(float));
+  cudaMalloc((void **)&t_b, arr_len * sizeof(float));
+  cudaMalloc((void **)&t_c, arr_len * sizeof(float));
 
   cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, n, n, n, &alpha, t_b, n, t_a, n,
               &beta, t_c, n);
